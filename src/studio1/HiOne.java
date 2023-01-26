fb0143a6a8daf504beb0f2ec6b6bf75a805b4a1c
zhiyangxue <zhiyangxue@localhost>
2023-01-26 13:09:03 -0600

    111

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -14,7 +14,7 @@ public class HiOne {
 		String name = in.nextLine();
 		//
 		// Below this line, enter code so that this program's output says
-		//      Hi, Pat.  How are you?
+	    System.out.println("Hi " + name + ". How are you?");
 		// if the value of name is "Pat"
 		//
 
